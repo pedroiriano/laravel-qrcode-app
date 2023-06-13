3ad1477ece950db570c7e6b0b4b1dd9b9e7dcb4f
Pedro Iriano <pdzeus83aw@gmail.com>
2023-06-13 13:54:51 +0700

    Display Users QrCode

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,24 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+        @if (!empty($users))
+            @foreach ($users as $user)    
+                <div class="col-md-4">
+                    <div class="card text-center mb-3">
+                        <div class="card-header">
+                            {{ $user ? $user->name : 'No Name' }}
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        <div class="card-body">
+                            @if (!empty($user->photo))
+                                <img src="{{ asset('storage/img/qr-code/'.$user->photo) }}" alt="User QR">
+                            @else
+                                <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(200)->generate('Default User')) !!}">
+                            @endif
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            @endforeach
+        @endif
     </div>
 </div>
 @endsection
