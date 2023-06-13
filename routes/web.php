a34309712b5b7e316e86d7a9efe6b67fba318001
Pedro Iriano <pdzeus83aw@gmail.com>
2023-06-13 15:31:32 +0700

    Add Auth Lib

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QrCodeController;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
