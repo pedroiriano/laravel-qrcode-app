69aeabeafcb1d5f6f7d4d41c322bdb62fb7af9f9
Pedro Iriano <pdzeus83aw@gmail.com>
2023-06-29 02:13:16 +0700

    Show QR to Landing Page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Rent;
 
 class PagesController extends Controller
 {
     public function index()
     {
-        return view('pages.index');
+        $rens = DB::table('rents')
+            ->join('stalls', 'rents.stall_id', '=', 'stalls.id')
+            ->join('merchants', 'rents.merchant_id', '=', 'merchants.id')
+            ->select('rents.*', 'stalls.stall_type', 'stalls.area as stall_area', 'merchants.identity as merchant_identity', 'merchants.name as merchant_name', 'merchants.phone as merchant_phone')
+            ->paginate(6);
+
+        return view('pages.index')->with('rens', $rens);
     }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -9,18 +9,18 @@
     <div class="container px-3">
         <h2 class="mb-4">Daftar Kios atau Los</h2>
         <div class="row gx-3">
-            @if (!empty($all_users))
-                @foreach ($all_users as $user)
+            @if (!empty($rens))
+                @foreach ($rens as $ren)
                     <div class="col-md-4">
                         <div class="card text-center mb-3">
                             <div class="card-header">
-                                {{ $user ? $user->name : 'No Name' }}
+                                {{ $ren ? $ren->location : 'No Location' }}
                             </div>
                             <div class="card-body">
-                                @if (!empty($user->photo))
-                                    <img src="{{ asset('storage/img/qr-code/'.$user->photo) }}" alt="User QR">
+                                @if (!empty($ren->qr))
+                                    <img src="{{ asset('storage/img/qr-code/'.$ren->qr) }}" alt="Rent QR">
                                 @else
-                                    <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(200)->generate('Default User')) !!}">
+                                    <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(200)->generate('Default Rent')) !!}">
                                 @endif
                             </div>
                         </div>
@@ -29,7 +29,7 @@
             @endif
         </div>
         <div class="d-flex justify-content-center">
-            {{ $all_users->links('pagination::bootstrap-5') }}
+            {{ $rens->links('pagination::bootstrap-5') }}
         </div>
     </div>
 </section>
