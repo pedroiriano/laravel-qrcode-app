034f1235b65751ca456a2a18669575af9e49a9be
Pedro Iriano <pdzeus83aw@gmail.com>
2023-07-05 02:04:11 +0700

    Export DB

--- database/laravel-qrcode-app.sql
+++ database/laravel-qrcode-app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 28, 2023 at 06:32 PM
+-- Generation Time: Jul 04, 2023 at 07:03 PM
 -- Server version: 5.7.36
 -- PHP Version: 8.0.13
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `failed_jobs`;
 CREATE TABLE IF NOT EXISTS `failed_jobs` (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `uuid` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -51,18 +51,21 @@ CREATE TABLE IF NOT EXISTS `merchants` (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `identity` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `phone` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `photo` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `merchants`
 --
 
-INSERT INTO `merchants` (`id`, `identity`, `name`, `phone`, `created_at`, `updated_at`) VALUES
-(1, '3274031610830006', 'Pedro Iriano', '08999002566', '2023-06-28 06:36:22', '2023-06-28 07:20:49');
+INSERT INTO `merchants` (`id`, `identity`, `name`, `address`, `phone`, `photo`, `created_at`, `updated_at`) VALUES
+(1, '3274031610830006', 'Pedro Iriano', 'Perumahan BTN Griya Cilimus Indah Blok K No. 14', '08999002566', 'pedro-iriano-1688496356.jpeg', '2023-07-04 11:06:19', '2023-07-04 11:45:56'),
+(2, '3274031207860006', 'Vernando Erison', 'Pesona Cilebut 2 Blok AB 2 No. 21', '085777308883', 'vernando-erison-1688496414.jpeg', '2023-07-04 11:08:22', '2023-07-04 11:46:54');
 
 -- --------------------------------------------------------
 
@@ -73,7 +76,7 @@ INSERT INTO `merchants` (`id`, `identity`, `name`, `phone`, `created_at`, `updat
 DROP TABLE IF EXISTS `migrations`;
 CREATE TABLE IF NOT EXISTS `migrations` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -87,12 +90,14 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
 (4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(7, '2023_06_24_065927_create_roles_table', 2),
-(8, '2023_06_24_070411_add_role_id_to_users_table', 2),
-(9, '2023_06_24_073441_add_status_to_users_table', 3),
-(10, '2023_06_26_024351_create_stalls_table', 4),
-(11, '2023_06_26_174014_create_merchants_table', 5),
-(12, '2023_06_28_154825_create_rents_table', 6);
+(5, '2023_06_24_065927_create_roles_table', 1),
+(6, '2023_06_24_070411_add_role_id_to_users_table', 1),
+(7, '2023_06_24_073441_add_status_to_users_table', 1),
+(8, '2023_06_26_024351_create_stalls_table', 1),
+(9, '2023_06_26_174014_create_merchants_table', 1),
+(10, '2023_06_28_154825_create_rents_table', 1),
+(11, '2023_07_04_153616_add_address_and_photo_to_merchants_table', 1),
+(12, '2023_07_04_185107_add_area_to_rents_table', 2);
 
 -- --------------------------------------------------------
 
@@ -102,8 +107,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 
 DROP TABLE IF EXISTS `password_resets`;
 CREATE TABLE IF NOT EXISTS `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   KEY `password_resets_email_index` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -117,9 +122,9 @@ CREATE TABLE IF NOT EXISTS `password_resets` (
 DROP TABLE IF EXISTS `personal_access_tokens`;
 CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_type` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `tokenable_id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
   `abilities` text COLLATE utf8mb4_unicode_ci,
   `last_used_at` timestamp NULL DEFAULT NULL,
@@ -142,6 +147,7 @@ CREATE TABLE IF NOT EXISTS `rents` (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `stall_id` bigint(20) NOT NULL,
   `merchant_id` bigint(20) NOT NULL,
+  `area` int(11) NOT NULL,
   `location` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `trade_type` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `qr` text COLLATE utf8mb4_unicode_ci,
@@ -155,8 +161,8 @@ CREATE TABLE IF NOT EXISTS `rents` (
 -- Dumping data for table `rents`
 --
 
-INSERT INTO `rents` (`id`, `stall_id`, `merchant_id`, `location`, `trade_type`, `qr`, `status`, `created_at`, `updated_at`) VALUES
-(1, 4, 1, 'PI-00001', 'Sembako', 'img-1687975635.png', 'Aktif', '2023-06-28 11:07:15', '2023-06-28 11:07:15');
+INSERT INTO `rents` (`id`, `stall_id`, `merchant_id`, `area`, `location`, `trade_type`, `qr`, `status`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 5, 'Pintu Masuk Utara', 'Sembako', 'img-1688497322.png', 'Aktif', '2023-07-04 12:02:02', '2023-07-04 12:02:02');
 
 -- --------------------------------------------------------
 
@@ -171,16 +177,15 @@ CREATE TABLE IF NOT EXISTS `roles` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `roles`
 --
 
 INSERT INTO `roles` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'Super Admin', '2023-06-24 00:22:21', '2023-06-24 00:22:21'),
-(2, 'Admin', '2023-06-24 00:22:34', '2023-06-24 00:22:34'),
-(3, 'Pengguna', '2023-06-24 00:22:42', '2023-06-24 00:22:42');
+(1, 'Super Admin', '2023-07-04 10:02:12', '2023-07-04 10:02:12'),
+(2, 'Admin', '2023-07-04 10:02:19', '2023-07-04 10:02:19');
 
 -- --------------------------------------------------------
 
@@ -204,11 +209,11 @@ CREATE TABLE IF NOT EXISTS `stalls` (
 --
 
 INSERT INTO `stalls` (`id`, `stall_type`, `area`, `retribution`, `created_at`, `updated_at`) VALUES
-(1, 'Kios', '0 - 5 m2', 3500, '2023-06-26 10:32:12', '2023-06-26 10:32:12'),
-(2, 'Kios', '6 - 10 m2', 4500, '2023-06-26 10:32:31', '2023-06-26 10:32:31'),
-(3, 'Kios', '11 - 15 m2', 5000, '2023-06-26 10:32:48', '2023-06-26 10:32:48'),
-(4, 'Kios', '16 - 20 m2', 5500, '2023-06-26 10:32:58', '2023-06-26 10:32:58'),
-(5, 'Los', 'n/a', 4000, '2023-06-26 10:33:11', '2023-06-26 10:33:11');
+(1, 'Kios', '0 - 5 m2', 3500, '2023-07-04 11:09:36', '2023-07-04 11:09:36'),
+(2, 'Kios', '6 - 10 m2', 4500, '2023-07-04 11:09:50', '2023-07-04 11:09:50'),
+(3, 'Kios', '11 - 15 m2', 5000, '2023-07-04 11:10:13', '2023-07-04 11:10:13'),
+(4, 'Kios', '16 - 20 m2', 5500, '2023-07-04 11:10:29', '2023-07-04 11:10:29'),
+(5, 'Los', 'n/a', 4000, '2023-07-04 11:10:49', '2023-07-04 11:10:49');
 
 -- --------------------------------------------------------
 
@@ -219,11 +224,10 @@ INSERT INTO `stalls` (`id`, `stall_type`, `area`, `retribution`, `created_at`, `
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
-  `photo` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `role_id` bigint(20) NOT NULL DEFAULT '1',
   `status` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Tidak Aktif',
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -237,8 +241,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `photo`, `password`, `role_id`, `status`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Admin', 'admin@mail.com', NULL, 'img-1687970458.png', '$2y$10$WVJ.HrMiiS9XmrEu0cjRmujnqXGDoefdDM2p4le0jD79S4ZKg2P4u', 1, 'Tidak Aktif', NULL, '2023-06-28 09:40:58', '2023-06-28 09:40:58');
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `role_id`, `status`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', 'admin@mail.com', NULL, '$2y$10$JaKHNyAb2mO8bpBwrymXSueOkHjpIsdrAj3.zawlmvx8OaWrCf0CO', 1, 'Aktif', NULL, '2023-07-04 10:01:00', '2023-07-04 10:01:00');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
