d5e097c7af0297806a94d4f729bc200b659f320f
Pedro Iriano <pdzeus83aw@gmail.com>
2023-07-18 05:46:35 +0700

    Show QR on Landing

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -11,9 +11,10 @@ class PagesController extends Controller
     public function index()
     {
         $rens = DB::table('rents')
-            ->join('stall_types', 'rents.stall_id', '=', 'stall_types.id')
+            ->join('stalls', 'rents.stall_id', '=', 'stalls.id')
             ->join('merchants', 'rents.merchant_id', '=', 'merchants.id')
-            ->select('rents.*', 'stall_types.stall_type', 'stall_types.area as stall_area', 'merchants.identity as merchant_identity', 'merchants.name as merchant_name', 'merchants.phone as merchant_phone')
+            ->leftJoin('stall_types', 'stalls.stall_type_id', '=', 'stall_types.id')
+            ->select('rents.*', 'stalls.stall_type_id', 'stalls.location', 'stalls.area as stall_area', 'merchants.identity as merchant_identity', 'merchants.name as merchant_name', 'merchants.phone as merchant_phone', 'stall_types.stall_type', 'stall_types.retribution as stall_retribution')
             ->paginate(6);
 
         return view('pages.index')->with('rens', $rens);
